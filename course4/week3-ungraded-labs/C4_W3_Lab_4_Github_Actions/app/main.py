dce4882b7dc5cce16eb691c802914e78c41cbf4b
ycai4591679 <caiying4591679.com>
2022-06-22 22:36:37 +0800

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+# use fastAPP for prediction
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
